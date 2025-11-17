807b128b7a57693801f5c166c27c1bb3ec9ce21e
Abhishek Choudhary <github@az21.simplelogin.com>
2025-11-17 22:09:27 +0530

    Create Profile.ps1

---
+++ windows/Documents/PowerShell/Profile.ps1
@@ -0,0 +1,15 @@
+# ~/Documents/PowerShell/Profile.ps1
+
+# Alias first party tools and commands
+New-Alias e Explorer
+New-Alias hash Get-FileHash
+New-Alias touch New-Item
+
+# Alias third party tools and commands
+New-Alias vi nvim
+New-Alias vim nvim
+
+# Extensions
+Invoke-Expression (&starship init powershell)
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+Set-Alias -Name cd -Value z -Option AllScope -Scope Global -Force
